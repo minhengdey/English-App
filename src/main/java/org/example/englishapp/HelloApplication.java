89e3ae3f2ec1e0e0cb4de219f9f57ca0b46ebf27
DucDat902204 <tptm18122004@gmail.com>
2024-11-08 21:41:15 +0700

    update

--- src/main/java/org/example/englishapp/HelloApplication.java
+++ src/main/java/org/example/englishapp/HelloApplication.java
@@ -2,7 +2,9 @@ package org.example.englishapp;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 import java.io.IOException;
