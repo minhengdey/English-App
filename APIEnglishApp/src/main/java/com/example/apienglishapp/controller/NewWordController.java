013dd639a066c5f07e26c3ebe76b464511f85326
minhengdey <dlminhanh272@gmail.com>
2024-11-12 17:28:54 +0700

    sua path new word

--- APIEnglishApp/src/main/java/com/example/apienglishapp/controller/NewWordController.java
+++ APIEnglishApp/src/main/java/com/example/apienglishapp/controller/NewWordController.java
@@ -2,9 +2,15 @@ package com.example.apienglishapp.controller;
 
 import com.example.apienglishapp.dto.request.NewWordRequest;
 import com.example.apienglishapp.dto.response.NewWordResponse;
+import com.example.apienglishapp.exception.AppException;
+import com.example.apienglishapp.exception.ErrorCode;
 import com.example.apienglishapp.service.NewWordService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,39 +30,76 @@ public class NewWordController {
     @Autowired
     private NewWordService newWordService;
 
-    @GetMapping (value = "/new_word/{id}/{userId}/user")
-    @PreAuthorize("hasRole('ADMIN') or (#userId.toString() == authentication?.token?.claims['sub'])")
-    public NewWordResponse getNewWordByUserIdAndId (@PathVariable("id") Long id, @PathVariable("userId") Long userId) {
-        return newWordService.findByUserIdAndId(userId, id);
+    @GetMapping (value = "/new_word/{id}")
+    @PreAuthorize("hasRole('ADMIN') or hasRole('USER')")
+    public NewWordResponse getNewWordByUserIdAndId (@PathVariable("id") Long id) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication instanceof JwtAuthenticationToken) {
+            JwtAuthenticationToken jwtToken = (JwtAuthenticationToken) authentication;
+            Jwt jwt = jwtToken.getToken();
+            return newWordService.findByUserIdAndId(Long.parseLong(jwt.getSubject()), id);
+        }
+        throw new AppException(ErrorCode.UNAUTHENTICATED);
     }
 
-    @GetMapping (value = "/new_word/topic/{topic}/{userId}/user")
-    @PreAuthorize("hasRole('ADMIN') or (#userId.toString() == authentication?.token?.claims['sub'])")
-    public List<NewWordResponse> getAllByTopicAndUserId (@PathVariable("topic") String topic, @PathVariable("userId") Long userId) {
-        return newWordService.getAllByTopicAndUserId(topic, userId);
+    @GetMapping (value = "/new_word/topic/{topic}")
+    @PreAuthorize("hasRole('ADMIN') or hasRole('USER')")
+    public List<NewWordResponse> getAllByTopicAndUserId (@PathVariable("topic") String topic) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication instanceof JwtAuthenticationToken) {
+            JwtAuthenticationToken jwtToken = (JwtAuthenticationToken) authentication;
+            Jwt jwt = jwtToken.getToken();
+            return newWordService.getAllByTopicAndUserId(topic, Long.parseLong(jwt.getSubject()));
+        }
+        throw new AppException(ErrorCode.UNAUTHENTICATED);
     }
 
-    @PostMapping (value = "/new_word/{userId}/user")
-    @PreAuthorize("hasRole('ADMIN') or (#userId.toString() == authentication?.token?.claims['sub'])")
-    public NewWordResponse create (@Valid @RequestBody NewWordRequest newWord, @PathVariable("userId") Long userId) {
-        return newWordService.create(newWord, userId);
+    @PostMapping (value = "/new_word")
+    @PreAuthorize("hasRole('ADMIN') or hasRole('USER')")
+    public NewWordResponse create (@Valid @RequestBody NewWordRequest newWord) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication instanceof JwtAuthenticationToken) {
+            JwtAuthenticationToken jwtToken = (JwtAuthenticationToken) authentication;
+            Jwt jwt = jwtToken.getToken();
+            return newWordService.create(newWord, Long.parseLong(jwt.getSubject()));
+        }
+        throw new AppException(ErrorCode.UNAUTHENTICATED);
     }
 
-    @PutMapping (value = "new_word/{id}/{userId}/user")
-    @PreAuthorize("hasRole('ADMIN') or (#userId.toString() == authentication?.token?.claims['sub'])")
-    public NewWordResponse update (@RequestBody NewWordRequest newWord, @PathVariable("id") Long id, @PathVariable("userId") Long userId) {
-        return newWordService.update(id, newWord, userId);
+    @PutMapping (value = "new_word/{id}")
+    @PreAuthorize("hasRole('ADMIN') or hasRole('USER')")
+    public NewWordResponse update (@RequestBody NewWordRequest newWord, @PathVariable("id") Long id) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication instanceof JwtAuthenticationToken) {
+            JwtAuthenticationToken jwtToken = (JwtAuthenticationToken) authentication;
+            Jwt jwt = jwtToken.getToken();
+            return newWordService.update(id, newWord, Long.parseLong(jwt.getSubject()));
+        }
+        throw new AppException(ErrorCode.UNAUTHENTICATED);
     }
 
-    @DeleteMapping (value = "new_word/{id}/{userId}/user")
-    @PreAuthorize("hasRole('ADMIN') or (#userId.toString() == authentication?.token?.claims['sub'])")
-    public void delete (@PathVariable("id") Long id, @PathVariable("userId") Long userId) {
-        newWordService.deleteByUserIdAndId(userId, id);
+    @DeleteMapping (value = "new_word/{id}")
+    @PreAuthorize("hasRole('ADMIN') or hasRole('USER')")
+    public void delete (@PathVariable("id") Long id) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication instanceof JwtAuthenticationToken) {
+            JwtAuthenticationToken jwtToken = (JwtAuthenticationToken) authentication;
+            Jwt jwt = jwtToken.getToken();
+            newWordService.deleteByUserIdAndId(Long.parseLong(jwt.getSubject()), id);
+        } else {
+            throw new AppException(ErrorCode.UNAUTHENTICATED);
+        }
     }
 
-    @GetMapping (value = "/new_word/{userId}/user")
-    @PreAuthorize("hasRole('ADMIN') or (#userId.toString() == authentication?.token?.claims['sub'])")
-    public List<NewWordResponse> getAllNewWordByUserId (@PathVariable("userId") Long userId) {
-        return newWordService.getAllNewWordByUserId(userId);
+    @GetMapping (value = "/new_word")
+    @PreAuthorize("hasRole('ADMIN') or hasRole('USER')")
+    public List<NewWordResponse> getAllNewWordByUserId () {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication instanceof JwtAuthenticationToken) {
+            JwtAuthenticationToken jwtToken = (JwtAuthenticationToken) authentication;
+            Jwt jwt = jwtToken.getToken();
+            return newWordService.getAllNewWordByUserId(Long.parseLong(jwt.getSubject()));
+        }
+        throw new AppException(ErrorCode.UNAUTHENTICATED);
     }
 }
