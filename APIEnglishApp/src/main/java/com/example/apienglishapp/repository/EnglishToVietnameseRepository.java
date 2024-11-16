73ee9d73eb67d74a60d3907e5c5b310a3b51969f
BiTiS2004 <117881913+sybui2004@users.noreply.github.com>
2024-11-16 15:08:34 +0700

    Update EnglishToVietnameseRepository.java

--- APIEnglishApp/src/main/java/com/example/apienglishapp/repository/EnglishToVietnameseRepository.java
+++ APIEnglishApp/src/main/java/com/example/apienglishapp/repository/EnglishToVietnameseRepository.java
@@ -2,11 +2,13 @@ package com.example.apienglishapp.repository;
 
 import com.example.apienglishapp.entity.EnglishToVietnameseEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
+import java.util.List;
 
 @Repository
 public interface EnglishToVietnameseRepository extends JpaRepository <EnglishToVietnameseEntity, String> {
-    Optional<EnglishToVietnameseEntity> findByWord(String word);
+    @Query("SELECT e FROM EnglishToVietnameseEntity e WHERE e.word = :word")
+    List<EnglishToVietnameseEntity> findByWord(String word);
 }
