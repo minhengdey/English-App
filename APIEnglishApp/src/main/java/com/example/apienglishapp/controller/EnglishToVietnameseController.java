06f2f59971b8efb4bc36bb0cdc574b5e5c762d26
BiTiS2004 <117881913+sybui2004@users.noreply.github.com>
2024-11-18 18:40:43 +0700

    Update EnglishToVietnameseController.java

--- APIEnglishApp/src/main/java/com/example/apienglishapp/controller/EnglishToVietnameseController.java
+++ APIEnglishApp/src/main/java/com/example/apienglishapp/controller/EnglishToVietnameseController.java
@@ -5,6 +5,9 @@ import com.example.apienglishapp.service.EnglishToVietnameseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.util.Collections;
 import java.util.List;
 
 @RestController
@@ -20,6 +23,12 @@ public class EnglishToVietnameseController {
     }
     @GetMapping("/byWord/{word}")
     public List<EnglishToVietnameseEntity> getEnglishToVietnameseByWord (@PathVariable("word") String word) {
-        return englishToVietnameseService.getEnglishToVietnameseByWord(word);
+        try {
+            String decodedWord = URLDecoder.decode(word, StandardCharsets.UTF_8);
+            return englishToVietnameseService.getEnglishToVietnameseByWord(decodedWord);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return Collections.emptyList();
+        }
     }
 }
