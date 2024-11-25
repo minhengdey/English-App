977da9d39839c6146598f81fb4af2b5e9e2137f5
BiTiS2004 <117881913+sybui2004@users.noreply.github.com>
2024-11-25 14:18:27 +0700

    Update VietnameseToEnglishService.java

--- APIEnglishApp/src/main/java/com/example/apienglishapp/service/VietnameseToEnglishService.java
+++ APIEnglishApp/src/main/java/com/example/apienglishapp/service/VietnameseToEnglishService.java
@@ -5,7 +5,10 @@ import com.example.apienglishapp.repository.VietnameseToEnglishRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class VietnameseToEnglishService {
@@ -17,11 +20,14 @@ public class VietnameseToEnglishService {
     }
     
     public List<VietnameseToEnglishEntity> getVietnameseToEnglishByWord(String word) {
-        List<VietnameseToEnglishEntity> result = vietnameseToEnglishRepository.findByWord(word);
-        if (result.isEmpty()) {
+        List<VietnameseToEnglishEntity> allWords = vietnameseToEnglishRepository.findAllWords();
+        List<VietnameseToEnglishEntity> results = allWords.stream()
+                .filter(entity -> URLEncoder.encode(entity.getWord(), StandardCharsets.UTF_8).equals(word))
+                .collect(Collectors.toList());
+        if (results.isEmpty()) {
             throw new RuntimeException("Từ này không có trong từ điển");
         }
-        return result;
+        return results;
     }
 
 }
